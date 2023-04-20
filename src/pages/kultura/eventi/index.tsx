b2d31be08018f55c3c30bf4c50760c06c4901ee2
Timotej Sofijanović <tsofijanovic@gmail.com>
2023-04-20 12:56:33 +0200

    events width on small devices fixed

--- src/pages/kultura/eventi/index.tsx
+++ src/pages/kultura/eventi/index.tsx
@@ -25,15 +25,15 @@ const EventsPage = () => {
   return (
     <Layout title="Eventi">
       <PageTitle title="Kalendar evenata" />
-      <div className="flex gap-6 my-12">
+      <div className="flex flex-col lg:flex-row gap-6 my-12">
         <EventCards
           events={events}
           loading={isLoading}
-          className="w-2/3"
+          className="w-full lg:w-2/3"
           classNameLoading="mt-12 w-2/3"
           classNameEmpty="mt-12 w-2/3"
         />
-        <div className="w-1/3"></div>
+        <div className="w-full lg:w-1/3"></div>
       </div>
     </Layout>
   );
