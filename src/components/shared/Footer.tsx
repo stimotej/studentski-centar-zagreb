8093ad59cca8ceb0650685ca85bc41352e1c0f30
SofijanovicTimotej <76438562+stimotej@users.noreply.github.com>
2023-07-22 13:48:31 +0200

    Added link to footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,8 +4,16 @@ import { FaInstagram, FaFacebookSquare } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-[#222222] text-light">
-      <div className="flex items-center justify-between container md:max-w-[80%] mx-auto px-6 py-4 md:px-0">
-        Studentski centar u Zagrebu | 2023
+      <div className="flex items-center flex-wrap gap-3 justify-between container md:max-w-[80%] mx-auto px-6 py-4 md:px-0">
+        <span>
+          Studentski centar u Zagrebu | 2023 | Izradio{" "}
+          <a
+            href="https://refresh.hr/"
+            className="hover:text-white/80 active:text-white/60 hover:underline"
+          >
+            Refresh.hr
+          </a>
+        </span>
         <div className="flex items-center gap-3">
           <a
             href="https://www.instagram.com/sczagreb/"
