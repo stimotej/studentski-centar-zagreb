d9a535a6163279a5a7778cb03a8988c51986aeee
Timotej Sofijanović <76438562+stimotej@users.noreply.github.com>
2024-07-29 17:14:21 +0200

    more specific macher in image loader

--- image-loader.ts
+++ image-loader.ts
@@ -1,5 +1,7 @@
 import convertToProxyUrl from "@/utils/convertToProxyUrl";
 
 export default function myImageLoader({ src }: { src: string }) {
-  return src.startsWith("http") ? convertToProxyUrl(src) : src;
+  return src.startsWith("http://161.53.174.14/wp-content/uploads")
+    ? convertToProxyUrl(src)
+    : src;
 }
