68312b208deb2964e4bfe554380e81835d85c2c4
Timotej Sofijanović <76438562+stimotej@users.noreply.github.com>
2026-09-10 14:28:38 +0200

    Add aaiedu logo next to login button

--- src/components/login-student/IzdavanjeUgovoraCard.tsx
+++ src/components/login-student/IzdavanjeUgovoraCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import ButtonLink from "../elements/ButtonLink";
 import Card from "../shared/Card";
 import { useUI } from "@/utils/ui";
@@ -15,9 +16,17 @@ const IzdavanjeUgovoraCard: React.FC<IzdavanjeUgovoraCardProps> = (props) => {
         {ui("login.eContracts")}
       </h4>
       <p className="mt-6 mb-2 text-light">{ui("login.aaiSystem")}</p>
-      <ButtonLink href="https://natjecaj.sczg.hr/student/Application">
-        AAI@EduHr
-      </ButtonLink>
+      <div className="flex flex-wrap items-center gap-4">
+        <ButtonLink href="https://natjecaj.sczg.hr/student/Application">
+          AAI@EduHr
+        </ButtonLink>
+        <Image
+          src="/aai-eduhr-logo.svg"
+          alt="AAI@EduHr"
+          width={105}
+          height={25}
+        />
+      </div>
       <p className="mt-6 mb-2 text-light">{ui("login.noAaiAccount")} </p>
       <ButtonLink href="https://natjecaj.sczg.hr/student/Login">
         {ui("login.goToSignIn")}
